60e57d86d11e4c892b695735e5e1c8c7c90b06dd
RaulCamposEH <raul.hernandezcps@uanl.edu.mx>
2022-11-16 18:35:14 -0600

    Update Trampa.h

--- v1dx11rr/Trampa.h
+++ v1dx11rr/Trampa.h
@@ -16,17 +16,25 @@ public:
 
 	float colRadio;
 
-	Trampa(GameModel* Models[], Camara* Camara, fvec3 position, float radio) {
-		mTrampaModels[0] = Models[0];
-		mTrampaModels[1] = Models[1];
+	Trampa(GameModel* Model1, GameModel* Model2, Camara* Camara, fvec3 position, float radio) {
+		mTrampaModels[0] = Model1;
+		mTrampaModels[1] = Model2;
 		mCamera = Camara;
 		this->position = position;
 		this->colRadio = radio;
 	}
 
 	void Draw(Camara* camara, float scale, float specForce) {
-		if (activated) mTrampaModels[1]->Draw(camara, scale, specForce);
-		else mTrampaModels[0]->Draw(camara, scale, specForce);
+		if (activated) { 
+			mTrampaModels[1]->setPos(position);
+			mTrampaModels[1]->setAltura(position.y);
+			mTrampaModels[1]->Draw(camara, scale, specForce); 
+		}
+		else {
+			mTrampaModels[0]->setPos(position);
+			mTrampaModels[0]->setAltura(position.y);
+			mTrampaModels[0]->Draw(camara, scale, specForce);
+		}
 	}
 
 	void Update(Gallina* gallina[], Player* jugador) {
@@ -37,7 +45,13 @@ public:
 				gallina[i]->SetFallInTrap();
 			}
 		}
-		//jugador->CajaDeColision->CheckSphereColission(position, colRadio);
+	}
+
+	void SetPos(fvec3 pos) {
+		position = pos; 
+	}
+	void SetAltura(float altura) {
+		position.y = altura;
 	}
 
 };
